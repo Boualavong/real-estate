0ad96eb819a19b9b668b1babfea0e0794d277a70
boualavong <boualavong.celine@gmail.com>
2017-03-03 00:29:34 +0100

    Commit 1

--- app.js
+++ app.js
@@ -1,128 +1,164 @@
-//importing modules (-> librairies)
+//importing modules
 var express = require( 'express' );
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
-var bodyParser=require('body-parser'); 
 
-//creating a new express server -> express est un module pour le créer
+//creating a new express server
 var app = express();
 
-//setting EJS as the templating engine -> template
+//setting EJS as the templating engine
 app.set( 'view engine', 'ejs' );
 
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
 
+
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
-//page d'accueil qui s'affiche par défaut
-//function est anonyme, utilisée par souci de performance (+ rapide). req=requete, res=reponse ( qui renvoie à la page d'accueil)
+//we don't make a function here to make the code faster
 app.get( '/', function ( req, res ) {
-res.render(__dirname+'/views/pages/index')
+    res.render( 'home', {
+		error: "",
+    });
 });
 
-app.use(bodyParser.urlencoded({
-	extended:true
-}));
-
-
-app.post('/scrape',function(req,res){
-	 const url= req.body.lien;
-
-    if(url!="")  //récupère url qu'on analyse
-	{ 
-	   getLBCData(url,res,getMAEstimation) //geetMAE fn call back qui s'execute une fois que getLBCData est executée
-    }
-    else
-    { //si url est pas rempli
-    	console.log("on est pas bon")
-       res.render('pages/index', { 
-          error:'Url is empty'
-          }); //j'ai un répertoire page avec index.html
-     }
-
-    // res.render( 'home', {
-    //     message: 'The Home Page!'
+app.get( '/process', function ( req, res ) {
+	const url = req.query.lbcUrl;
+	if (url) {
+		getLBCData(url, res, getMAEstimation)
+	}
+	else {
+		res.render( 'home', {
+			error: 'Url is empty'
+		});
+	} 
 });
-   
 
 
-function getLBCData(lbcUrl,routeResponse, callback)
-{
-	request( lbcUrl,function(error,response,html)
-	{
-		if(!error)
-		{
-			const lcbData=parseLBCData(html);
-
-			if(lbcData) //si on extrait les données, callback est appelée
-			 {
-				console.log('LBCData:',lbcData)//affiche dans la console
-				callback(lbcData,routeResponse)
-			 }
-
-			else
-			 {
-				routeResponse.render('pages/index',
-					{error:'No data found'});
-			 }
+function getLBCData( lbcUrl, routeResponse, callback){
+	request( lbcUrl, function (error,response, html){
+		if (!error) {
+			let $ = cheerio.load(html);
+			const lbcData = parseLBCData (html)
+			if (lbcData){
+				console.log('LBC_Data:',lbcData)
+				callback(lbcData, routeResponse)
+			}
+			else{
+				routeResponse.render('home', {
+					error: 'No data found'
+				});	
+			}
 		}
-
-		else
-		{
-			routeResponse.render('pages/index',
-			{error:'Error loading the given URL'});
+		else{
+			routeResponse.render('home', {
+				error:'Error loading the given URL'
+			});	
 		}
-
-		
 	});
 }
 
+function parseLBCData (html) {
+	
+	const $ = cheerio.load(html);
+	const lbcDataArray = $('section.properties span.value')
 
-function parseLBCData(html)
-{
-	const $ =cheerio.load(html)
-
-	const lbcDataArray=$ ('#adview > section > section > section.properties.lineNegative > div:nth-child(7) > h2 > span.value')
-
-	//toutes les vleurs des noeuds "span" qui sont fils de section.properties
-	//stocke dans un tableau
-	//récupérer les données à partir du tableau
-
-	return lbcData={
-		type: ( $ ( lbcDataArray )
-		        .text().replace(/\s/g, ''),10 )
-
+	return lbcData = {
+		price: parseInt($(lbcDataArray.get(0)).text().replace(/\s/g,''),10),
+		city: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\s/g,'-').replace(/\-\d+/,''),
+		postalCode: $(lbcDataArray.get(1)).text().trim().toLowerCase().replace(/\D|\-/g,''),
+		type: $(lbcDataArray.get(2)).text().trim().toLowerCase(),
+		surface: parseInt($(lbcDataArray.get(4)).text().replace(/\s/g,''),10)
 	}
 }
 
 
-function getMAEstimation(lbcData, routeResponse)
-{
-	if( lbcData.city && lbcData.postalCode && lbcData.surface && lbcData.price)
-	{
-		const url='https://wwww.meilleursagents.com/prix-immobilier/{city}-{postalCode}/'.replace('{city}',
-			lbcData.city.replace(/\_/g,'-') )
-		.replace( '{postalCode}', lbcData.postalCode);
-
-		console.log('MA URL: ', url)
-
-		request ( url, function (error,response, html)
-			{
-				if(!error){ let $ = cheerio.load(html);}//module pour parser le doc html (parser=analyser)}
+function getMAEstimation (lbcData, routeResponse ) {
+	if (lbcData.city && lbcData.surface &&  lbcData.price){
+		//instancie les paramètres avec les valeurs de lbcData pour city et postaleCode
+		const url = 'https://www.meilleursagents.com/prix-immobilier/{city}-{postalCode}/'.replace('{city}',lbcData.city.replace(/\_/g,'-')).replace('{postalCode}',lbcData.postalCode);
+		
+		console.log('MA_URL:', url)
+	
+		request(url, function(error, response, html) {
+			if (!error){
+				let $ = cheerio.load(html);
+				//console.log($( 'meta[name=description]').get());
+				//console.log($( 'meta[name=description]').get()[0].attribs);
+				//console.log($( 'meta[name=description]').get()[0].attribs.content );
+				if($( 'meta[name=description]').get().length===1 && $( 'meta[name=description]').get()[0].attribs && $( 'meta[name=description]').get()[0].attribs.content ) {
+					const maData = parseMAData ($( 'meta[name=description]').get()[0].attribs.content)
+					if (maData){
+						console.log('MA_Data:',maData);//affiche dans la console
+						if (maData.priceAppart && maData.priceHouse){
+							routeResponse.render( 'home', {
+								error: "No error",
+								data: {
+									lbcData,
+									maData,
+									deal : {
+										good : isGoodDeal(lbcData,maData),
+									},
+									
+								}
+							});
+							console.log(isGoodDeal(lbcData,maData));
+						}
+						else{
+							routeResponse.render('home', {
+								error:'Unvalid maData'
+							});
+						}
+					}
+					else{
+						routeResponse.render('home', {
+							error:'No data found'
+						});	
+					}
+				}
 			}
-
-		       )
+			else{
+				routeResponse.render('home', {
+					error:'Error loading the given URL'
+				});	
+			}
+		});	
+	}
+	else{
+			routeResponse.render('home', {
+				error:'Unvalid LBC_Data, please change URL'
+			});
 	}
 }
 
-function isGoodDeal(lbcData,maData)
-{
-	const adPricePerSqM=Math.round(lbcData.price / lbcData.surface)
-	const maPrice = lbcData.type === 'appartement' ? maData.priceAppart:maData.priceHouse
+function parseMAData (html) {
+	
+	const priceAppartRegex = /\bappartement\b : (\d+) €/mi
+	const priceHouseRegex = /\bmaison\b : (\d+) €/mi
+	
+	if(html)
+	{
+		const priceAppart = priceAppartRegex.exec( html ) && priceAppartRegex.exec( html ).length === 2 ? priceAppartRegex.exec( html )[1] :0
+		const priceHouse = priceHouseRegex.exec( html ) && priceHouseRegex.exec( html ).length === 2 ? priceHouseRegex.exec( html )[1] :0
+		if (priceAppart && priceHouse){
+			return maData = {
+				priceAppart,
+				priceHouse
+			}
+		}
+	}
+	
+}
 
+//to get to know if it's a good deal or not
+function isGoodDeal (lbcData, maData) {
+	
+	console.log("dans good deal");
+	const adPricePerSqM = Math.round(lbcData.price/lbcData.surface) 
+	
+	const maPrice = lbcData.type ==='appartement' ?maData.priceAppart : maData.priceHouse
+	
 	return adPricePerSqM < maPrice
 }
-
 
 
 //launch the server on the 3000 port
